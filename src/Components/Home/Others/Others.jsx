54c0848649d54cd658702e2b44225b232067f9d6
MASUMHD <mrmasum2k4@gmail.com>
2024-12-22 21:29:59 +0600

    right site complate

--- src/Components/Home/Others/Others.jsx
+++ src/Components/Home/Others/Others.jsx
@@ -4,7 +4,7 @@ import Right from "./Right";
 const Others = () => {
   return (
     <div>
-      <div className="flex gap-10 mt-16 mb-20">
+      <div className="flex-row lg:flex gap-10 mt-16 mb-20">
         <div className="flex-1 ">
           <Left />
         </div>
--- src/Components/Home/Others/Right.jsx
+++ src/Components/Home/Others/Right.jsx
@@ -1,135 +1,121 @@
-import { useState } from "react";
-import {
-  FaVideo,
-  FaClinicMedical,
-  FaCommentAlt,
-  FaCalendarAlt,
-} from "react-icons/fa"; // React Icons
+import { FaCalendarAlt } from "react-icons/fa";
+import { IoIosArrowDropleft, IoIosArrowDropright } from "react-icons/io";
 
 const Right = () => {
-  const [selectedSession, setSelectedSession] = useState("video");
-  const [selectedTime, setSelectedTime] = useState("");
 
   return (
-    <div className=" p-6 border rounded-xl">
-      {/* Appointment Fee */}
+    <main className=" p-6 border rounded-xl">
       <div className="flex justify-between items-center mb-6 border rounded-xl px-6 py-3">
         <h2 className="text-lg font-semibold">Appointment Fee</h2>
         <span className="text-xl font-semibold text-green-600">₹699.00</span>
       </div>
 
-      {/* Session Selection */}
       <div className="mb-6">
         <h3 className="text-lg font-semibold mb-3 flex items-center gap-14">
           <span>Select your mode of session</span>{" "}
           <hr className="w-2/4 border-gray-300" />
         </h3>
         <div className="flex gap-4 mt-6">
-          <button
-            className={`flex items-center gap-2 p-3 border rounded-lg w-full text-sm ${
-              selectedSession === "in-clinic"
-                ? "bg-green-100 text-green-700 border-green-700"
-                : "text-gray-600 border-gray-300"
-            }`}
-            onClick={() => setSelectedSession("in-clinic")}
-          >
-            <FaClinicMedical />
-            In-Clinic <span className="ml-auto">45 Mins</span>
+          <button className="items-center gap-2 p-3 border rounded-lg w-full text-sm">
+            <p className="font-semibold mb-1">In-Clinic</p>{" "}
+            <p className="ml-auto text-gray-500">45 Mins</p>
           </button>
-          <button
-            className={`flex items-center gap-2 p-3 border rounded-lg w-full text-sm ${
-              selectedSession === "video"
-                ? "bg-green-100 text-green-700 border-green-700"
-                : "text-gray-600 border-gray-300"
-            }`}
-            onClick={() => setSelectedSession("video")}
-          >
-            <FaVideo />
-            Video <span className="ml-auto">45 Mins</span>
+          <button className="items-center gap-2 p-3 border rounded-lg w-full text-sm bg-green-100 text-green-700 border-green-700">
+            <p className="font-semibold mb-1">Video</p>{" "}
+            <p className="ml-auto text-gray-500">45 Mins</p>
           </button>
-          <button
-            className={`flex items-center gap-2 p-3 border rounded-lg w-full text-sm ${
-              selectedSession === "chat"
-                ? "bg-green-100 text-green-700 border-green-700"
-                : "text-gray-600 border-gray-300"
-            }`}
-            onClick={() => setSelectedSession("chat")}
-          >
-            <FaCommentAlt />
-            Chat <span className="ml-auto">10 Mins</span>
+          <button className="items-center gap-2 p-3 border rounded-lg w-full text-sm ">
+            <p className="font-semibold mb-1">Chat</p>{" "}
+            <p className="ml-auto text-gray-500">45 Mins</p>
           </button>
         </div>
       </div>
 
-      {/* Time Slot Selection */}
       <div>
-        <h3 className="text-lg font-semibold mb-3 flex items-center gap-8">Pick a time slot <hr className="w-96 border-gray-300" /><FaCalendarAlt className="text-gray-500 " /></h3>
-        <div className="flex items-center gap-3 mb-6">
-          <FaCalendarAlt className="text-gray-500" />
-          <div className="flex gap-2 overflow-x-auto">
-            <button className="p-2 border rounded-lg bg-green-100 text-green-700">
-              Mon, 10 Oct <span className="text-xs">(10 slots)</span>
-            </button>
-            <button className="p-2 border rounded-lg text-red-500 border-red-500">
-              Tue, 11 Oct <span className="text-xs">(2 slots)</span>
-            </button>
-            <button className="p-2 border rounded-lg text-yellow-500 border-yellow-500">
-              Wed, 12 Oct <span className="text-xs">(5 slots)</span>
-            </button>
+        <h3 className="text-lg font-semibold mb-3 flex items-center justify-between gap-8">
+          Pick a time slot <hr className="w-80 border-gray-300" />
+          <div className=" p-2 rounded-full border-2 ">
+            <FaCalendarAlt className="text-gray-500 " />
+          </div>
+        </h3>
+
+        <div className="h-28 flex items-center justify-center overflow-y-auto border rounded-2xl">
+          <div className="flex items-center  gap-3 ">
+            <div>
+              <IoIosArrowDropleft className="text-gray-500 h-7 w-7" />
+            </div>
+
+            <div className="flex gap-2  ">
+              <button className="p-2 border rounded-lg bg-green-100 text-green-700 w-60">
+                Mon, 10 Oct <br /> <span className="text-xs">(10 slots)</span>
+              </button>
+              <button className="p-2 border rounded-lg text-red-500 border-red-500 w-1/2">
+                Tue, 11 Oct <br /> <span className="text-xs">(2 slots)</span>
+              </button>
+              <button className="p-2 border rounded-lg text-yellow-500 border-yellow-500 w-1/2">
+                Wed, 12 Oct <br /> <span className="text-xs">(5 slots)</span>
+              </button>
+            </div>
+
+            <div>
+              <IoIosArrowDropright className="text-gray-500 h-7 w-7" />
+            </div>
           </div>
         </div>
 
-        {/* Time Slots */}
-        <div>
-          <h4 className="font-semibold mb-4">Morning</h4>
-          <div className="flex gap-3 mb-4">
-            {[
-              "09:00 AM",
-              "09:30 AM",
-              "10:00 AM",
-              "10:15 AM",
-              "10:45 AM",
-              "11:00 AM",
-            ].map((time, index) => (
-              <button
-                key={index}
-                className={`p-2 border rounded-lg ${
-                  selectedTime === time
-                    ? "bg-green-100 text-green-700 border-green-700"
-                    : "text-gray-600 border-gray-300"
-                }`}
-                onClick={() => setSelectedTime(time)}
-              >
-                {time}
-              </button>
-            ))}
-          </div>
-          <h4 className="font-semibold mb-3">Evening</h4>
+        <div className="mt-6">
+          <h4 className="font-semibold mb-3">Morning</h4>
           <div className="flex gap-3">
-            {["04:00 PM", "04:15 PM", "04:30 PM", "04:45 PM", "05:15 PM"].map(
-              (time, index) => (
-                <button
-                  key={index}
-                  className={`p-2 border rounded-lg ${
-                    selectedTime === time
-                      ? "bg-green-100 text-green-700 border-green-700"
-                      : "text-gray-600 border-gray-300"
-                  }`}
-                  onClick={() => setSelectedTime(time)}
-                >
-                  {time}
-                </button>
-              )
-            )}
+            <div className="flex h-16 w-32 p-4 border rounded-xl items-center justify-center text-gray-600 border-gray-300">
+              <p>09:00 AM</p>
+            </div>
+            <div className="flex h-16 w-32 p-4 border rounded-xl items-center justify-center text-gray-600 border-gray-300">
+              <p>09:30 AM</p>
+            </div>
+            <div className="flex h-16 w-32 p-4 border rounded-xl items-center justify-center text-gray-600 border-gray-300">
+              <p>10:00 AM</p>
+            </div>
+            <div className="flex h-16 w-32 p-4 border rounded-xl items-center justify-center text-gray-600 border-gray-300">
+              <p>10:15 AM</p>
+            </div>
+          </div>
+          <div className="flex gap-3 mt-3">
+            <div className="flex h-16 w-32 p-4 border rounded-xl items-center justify-center text-gray-600 border-gray-300">
+              <p>10:45 AM</p>
+            </div>
+            <div className="flex h-16 w-32 p-4 border rounded-xl items-center justify-center text-gray-200 border-gray-600 bg-[#3A643B]">
+              <p>11:00 AM</p>
+            </div>
+          </div>
+          <div className="mt-6">
+            <h4 className="font-semibold mb-3">Evening</h4>
+            <div className="flex gap-3">
+              <div className="flex h-16 w-32 p-4 border rounded-xl items-center justify-center text-gray-600 border-gray-300">
+                <p>09:00 AM</p>
+              </div>
+              <div className="flex h-16 w-32 p-4 border rounded-xl items-center justify-center text-gray-600 border-gray-300">
+                <p>09:30 AM</p>
+              </div>
+              <div className="flex h-16 w-32 p-4 border rounded-xl items-center justify-center text-gray-600 border-gray-300">
+                <p>10:00 AM</p>
+              </div>
+              <div className="flex h-16 w-32 p-4 border rounded-xl items-center justify-center text-gray-600 border-gray-300">
+                <p>10:15 AM</p>
+              </div>
+            </div>
+            <div className="flex gap-3 mt-3">
+              <div className="flex h-16 w-32 p-4 border rounded-xl items-center justify-center text-gray-600 border-gray-300">
+                <p>10:45 AM</p>
+              </div>
+            </div>
           </div>
         </div>
       </div>
 
-      {/* Make Appointment */}
       <button className="w-full mt-8 py-3 bg-green-700 text-white font-semibold rounded-lg hover:bg-green-600">
         Make An Appointment
       </button>
-    </div>
+    </main>
   );
 };
 
